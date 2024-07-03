181a3d41122606bb3a19911273f4f9f495690293
syedaaryan <syedaaryans@gmail.com>
2024-07-03 18:49:17 +0530

    Added Home view

---
+++ app/src/main/java/com/example/financemanagement/view/HomeView.kt
@@ -0,0 +1,9 @@
+package com.example.financemanagement.view
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeView (){
+    Text(text = "Home")
+}
