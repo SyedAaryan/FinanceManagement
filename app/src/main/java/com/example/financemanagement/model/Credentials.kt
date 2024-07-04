842197470f86e8ff31087c59871564ff7348f89e
syedaaryan <syedaaryans@gmail.com>
2024-07-04 14:15:40 +0530

    Add google-services.json to .gitignore

---
+++ app/src/main/java/com/example/financemanagement/model/Credentials.kt
@@ -0,0 +1,6 @@
+package com.example.financemanagement.model
+
+data class Credentials(
+    val username: String,
+    val password: String
+)
---
+++ app/src/main/java/com/example/financemanagement/services/Authentication.kt
@@ -0,0 +1,82 @@
+package com.example.financemanagement.services
+
+import android.app.Activity
+import android.content.ContentValues.TAG
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseUser
+
+class Authentication : Activity (){
+    private lateinit var auth: FirebaseAuth
+
+    public override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        auth = Firebase.auth
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            reload()
+        }
+    }
+
+    private fun reload() {
+
+    }
+
+    private fun createAccount(username: String, password: String) {
+        // [START create_user_with_email]
+        auth.createUserWithEmailAndPassword(username, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithUsername:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithUsername:failure", task.exception)
+                    Toast.makeText(
+                        baseContext,
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                    updateUI(null)
+                }
+            }
+        // [END create_user_with_email]
+    }
+
+    private fun signIn(username: String, password: String) {
+        // [START sign_in_with_email]
+        auth.signInWithEmailAndPassword(username, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithUsername:success")
+                    val user = auth.currentUser
+                    updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithUsername:failure", task.exception)
+                    Toast.makeText(
+                        baseContext,
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                    updateUI(null)
+                }
+            }
+
+    }
+
+    private fun updateUI(user: FirebaseUser?) {
+    }
+}
