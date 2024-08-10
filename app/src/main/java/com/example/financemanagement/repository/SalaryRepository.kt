3f93e7e74e43fe1ba321cd8dc0c944bda2177656
syedaaryan <syedaaryans@gmail.com>
2024-08-10 18:14:34 +0530

    Added Feature where salary gets updated after one month (Beta)

--- app/src/main/java/com/example/financemanagement/repository/SalaryRepository.kt
+++ app/src/main/java/com/example/financemanagement/repository/SalaryRepository.kt
@@ -1,5 +1,6 @@
 package com.example.financemanagement.repository
 
+
 import com.example.financemanagement.services.FirebaseService
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
@@ -61,6 +62,67 @@ object SalaryRepository {
                         onFailure(Exception("Failed to get salary"))
                     }
                 })
+        }
+    }
+
+    suspend fun getSalaryDate(): Long {
+        val user = FirebaseService.user
+        if (user != null) {
+            val uid = user.uid
+            try {
+                val snapshot = database.getReference("Users/$uid/Salary and Date")
+                    .get().
+                    await()
+
+                val dateMillis = snapshot.child("Date").getValue(Long::class.java) ?: 0L
+                return dateMillis
+
+            } catch (e: Exception) {
+                throw e
+            }
+        } else {
+            throw IllegalStateException("User is not authenticated.")
+        }
+    }
+
+    //This function is used to get the salary that is stored in the "Salary and Date" from the DB
+    suspend fun getConstantSalary(): Int {
+        val user = FirebaseService.user
+        if (user != null) {
+            val uid = user.uid
+            try {
+                val snapshot = database.getReference("Users/$uid/Salary and Date")
+                    .get().
+                    await()
+
+                val constSalary = snapshot.child("Salary").getValue(Int::class.java) ?: 0
+                return constSalary
+
+            } catch (e: Exception) {
+                throw e
+            }
+        } else {
+            throw IllegalStateException("User is not authenticated.")
+        }
+    }
+
+
+    suspend fun updateSalaryDateAndRemainingSalary(newDate: Long, newAmount: Int) {
+        val user = FirebaseService.user
+        if (user != null) {
+            val uid = user.uid
+            try {
+                database.getReference("Users/$uid/Salary and Date/Date")
+                    .setValue(newDate)
+                    .await()
+                database.getReference("Users/$uid/Remaining Salary")
+                    .setValue(newAmount)
+                    .await()
+            } catch (e: Exception) {
+                throw e
+            }
+        } else {
+            throw IllegalStateException("User is not authenticated.")
         }
     }
 }
--- app/src/main/java/com/example/financemanagement/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/financemanagement/viewmodel/HomeViewModel.kt
@@ -4,8 +4,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.financemanagement.repository.SalaryRepository
 import com.example.financemanagement.services.FirebaseService
+import kotlinx.coroutines.launch
+import java.time.Instant
+import java.time.LocalDate
+import java.time.ZoneId
 
 class HomeViewModel : ViewModel() {
 
@@ -13,6 +18,8 @@ class HomeViewModel : ViewModel() {
 
     init {
         fetchSalary()
+        checkAndUpdateSalary()
+
     }
 
     private fun fetchSalary() {
@@ -29,4 +36,22 @@ class HomeViewModel : ViewModel() {
     fun signOut() {
         FirebaseService.auth.signOut()
     }
+
+    private fun checkAndUpdateSalary() {
+        viewModelScope.launch {
+            try {
+                val storedDate = SalaryRepository.getSalaryDate()
+                val storedLocalDate = Instant.ofEpochMilli(storedDate).atZone(ZoneId.systemDefault()).toLocalDate()
+                val currentDate = LocalDate.now()
+                if (currentDate >= storedLocalDate.plusDays(30)) {
+                    val constSalary = SalaryRepository.getConstantSalary()
+                    SalaryRepository.updateSalaryDateAndRemainingSalary(currentDate.toEpochDay() * 86400000, constSalary)
+                }
+            } catch (e: Exception) {
+                // Handle exception
+            }
+        }
+
+    }
+
 }
